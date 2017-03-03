7b56f51fa2791e2c05f7d5a14231e13c7cf37e53
Riley Watts <rileywatts.yes@gmail.com>
2017-03-03 09:44:57 -0800

    add input forms to html, object constructors, javascript functionality

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,30 @@ function Customer(firstName, lastName, pizzas) {
   this.last = lastName;
   this.pizzas= pizzas;
 }
+
+toppings=[];
+
+$(function(){
+
+
+
+  $("#formTwo").submit(function(event) {
+    event.preventDefault();
+    var size = $("input:checkbox[name=premTopping]:checked").val();
+    console.log(size);
+  })
+
+
+
+
+
+
+  // pushing user toppings to an array
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    $("input:checkbox[name=topping]:checked").each(function(){
+      var pizzaToppings = $(this).val();
+      toppings.push(pizzaToppings);
+    });
+  });
+});
